d5d99c38235fc5daa3ebd86346f69bad2383a0b4
Arash <47273556+arashnrim@users.noreply.github.com>
2021-11-24 14:29:10 +0800

    Update Header and Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaHeart } from "react-icons/fa";
 
 import Socials from "./Socials";
@@ -7,9 +6,9 @@ import InlineLink from "./InlineLink";
 const Footer = () => (
   <footer className="justify-center items-center flex flex-col space-y-5 md:space-y-10 min-h-[33vh] min-w-screen bg-black text-center rounded-t-3xl">
     <p className="px-10 md:px-20 sm:text-xl md:text-xl">
-      Made with <FaHeart className="inline h-[24px]" /> by Arash Nur Iman;{" "}
-      <InlineLink link="https://github.com/arashnrim/website" redirect={true}>
-        open-sourced on GitHub
+      Made with <FaHeart className="inline h-[24px]" /> by{" "}
+      <InlineLink link="https://arash.codes" redirect={true}>
+        Arash Nur Iman
       </InlineLink>
       .
     </p>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Link from "next/link";
 
 const Header = () => {
@@ -6,8 +5,7 @@ const Header = () => {
     <header className="fixed flex z-50 p-5 items-center w-screen lg:h-20 bg-black bg-opacity-[0.025] backdrop-filter backdrop-blur-sm">
       <Link href="/" passHref={true}>
         <span className="p-3 transition cursor-pointer w-min bg-opacity-5 hover:opacity-75">
-          <span className="font-mono text-2xl font-bold">#</span>
-          <span className="font-sans text-2xl font-bold">A</span>
+          <span className="font-mono text-2xl italic font-bold">fn()</span>
         </span>
       </Link>
     </header>
