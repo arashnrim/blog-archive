8969c1dc7b4ba7dc1de928c4ef9bc05a3eca000c
Arash <47273556+arashnrim@users.noreply.github.com>
2021-11-24 12:13:01 +0800

    Add fonts and configure Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['"Clash Display"'],
+        sans: ["Inter"],
+        mono: ["'JetBrains Mono'"],
+      },
+      colors: {
+        gray: colors.trueGray,
+      },
+    },
   },
   variants: {
     extend: {},
