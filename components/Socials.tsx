45b44b17d8103ecde56171c08347e8ccfd9bf111
Arash <47273556+arashnrim@users.noreply.github.com>
2021-12-10 20:01:20 +0800

    Update Socials

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,48 +1,71 @@
-import React from "react";
-import { SiGithub, SiLinkedin, SiPolywork } from "react-icons/si";
-import { FaEnvelope } from "react-icons/fa";
+import { FaEnvelope, FaBlog } from "react-icons/fa";
+import { SiFigma, SiGithub, SiLinkedin } from "react-icons/si";
+
+const socials = [
+  {
+    name: "GitHub",
+    icon: (
+      <SiGithub className="w-full h-full transition-opacity duration-200 hover:opacity-75" />
+    ),
+    link: "https://github.com/arashnrim",
+  },
+  {
+    name: "blog",
+    icon: (
+      <FaBlog className="w-full h-full transition-opacity duration-200 hover:opacity-75" />
+    ),
+    link: "https://blog.arash.codes",
+  },
+  {
+    name: "LinkedIn",
+    icon: (
+      <SiLinkedin className="w-full h-full transition-opacity duration-200 hover:opacity-75" />
+    ),
+    link: "https://linkedin.com/in/arashnrim",
+  },
+  {
+    name: "Figma",
+    icon: (
+      <SiFigma className="w-full h-full transition-opacity duration-200 hover:opacity-75" />
+    ),
+    link: "https://figma.com/@arashnrim",
+  },
+  {
+    name: "email",
+    icon: (
+      <FaEnvelope className="w-full h-full transition-opacity duration-200 hover:opacity-75" />
+    ),
+    link: "mailto:hello@arashnrim.me",
+  },
+];
 
 const Socials = () => (
   <nav className="flex flex-row items-center justify-center mt-5 space-x-6 lg:justify-start lg:items-start">
-    <a
-      href="https://github.com/arashnrim"
-      target="_blank"
-      rel="noreferrer"
-      className="w-8 h-8 md:w-10 sm:h-10"
-      title="Go to GitHub profile"
-      aria-label="Go to GitHub profile"
-    >
-      <SiGithub className="w-full h-full transition-opacity hover:opacity-75" />
-    </a>
-    <a
-      href="https://timeline.arash.codes"
-      target="_blank"
-      rel="noreferrer"
-      className="w-8 h-8 md:w-10 sm:h-10"
-      title="Go to Polywork profile"
-      aria-label="Go to Polywork profile"
-    >
-      <SiPolywork className="w-full h-full transition-opacity hover:opacity-75" />
-    </a>
-    <a
-      href="https://www.linkedin.com/in/arashnrim/"
-      target="_blank"
-      rel="noreferrer"
-      className="w-8 h-8 md:w-10 sm:h-10"
-      title="Go to LinkedIn profile"
-      aria-label="Go to LinkedIn profile"
-    >
-      <SiLinkedin className="w-full h-full transition-opacity hover:opacity-75" />
-    </a>
-
-    <a
-      href="mailto:hello@arashnrim.me"
-      className="w-8 h-8 md:w-10 sm:h-10"
-      title="Contact via email"
-      aria-label="Contact via email"
-    >
-      <FaEnvelope className="w-full h-full transition-opacity hover:opacity-75" />
-    </a>
+    {socials.map((social) => (
+      <a
+        href={social.link}
+        target="_blank"
+        rel="noreferrer"
+        className="w-8 h-8 md:w-10 sm:h-10"
+        title={
+          social.name === "email"
+            ? "Contact via email"
+            : social.name === "blog"
+            ? "Go to blog"
+            : `Go to ${social.name} profile`
+        }
+        aria-label={
+          social.name === "email"
+            ? "Contact via email"
+            : social.name === "blog"
+            ? "Go to blog"
+            : `Go to ${social.name} profile`
+        }
+        key={social.name}
+      >
+        {social.icon}
+      </a>
+    ))}
   </nav>
 );
 
