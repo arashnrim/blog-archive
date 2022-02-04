d14d3438405b338a3da7356b1a7140882398dd1d
Arash <47273556+arashnrim@users.noreply.github.com>
2022-02-04 19:57:51 +0800

    Add About page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <header className="fixed flex z-50 p-5 items-center w-screen lg:h-20 bg-gradient-to-b from-black backdrop-filter backdrop-blur-sm">
+    <header className="fixed z-50 flex items-center w-screen h-20 p-5 lg:flex-col lg:h-screen lg:w-28 bg-gradient-to-b lg:bg-gradient-to-r lg:space-y-6 from-black backdrop-filter backdrop-blur-sm">
       <Link href="/" passHref={true}>
         <span className="p-3 transition cursor-pointer w-min bg-opacity-5 hover:opacity-75">
           <span className="font-mono text-2xl italic">fn()</span>
         </span>
       </Link>
+      {(router.asPath === "/" ||
+        router.asPath === "/about" ||
+        router.asPath === "/404") && (
+        <nav className="flex flex-row justify-center pl-5 space-x-5 lg:items-center lg:flex-col lg:pl-0 lg:space-x-0 lg:space-y-8 lg:w-full">
+          <Link href="/about" passHref>
+            <a className="text-xl text-center underline-offset-0 lg:vertical-text">
+              about
+            </a>
+          </Link>
+        </nav>
+      )}
     </header>
   );
 };
---
+++ pages/about.tsx
@@ -0,0 +1,70 @@
+import Hero from "../components/Hero";
+import Layout from "../components/Layout";
+import Image from "next/image";
+import Link from "next/link";
+import profilePicture from "../public/arash.jpg";
+
+const About = () => {
+  return (
+    <Layout className="text-lg sm:text-xl">
+      <Hero />
+      <section className="py-10 lg:pt-20">
+        <div className="prose prose-lg text-white prose-invert sm:prose-xl md:prose-2xl max-w-none">
+          <object className="float-left w-1/3 max-w-xs pb-2 pr-10 lg:pb-8 lg:pr-20">
+            <Image
+              src={profilePicture}
+              alt="Arash Nur Iman"
+              className="rounded-xl"
+            />
+          </object>
+          <p>
+            Hello there! My name is Arash Nur Iman. I&apos;m a student developer
+            from Singapore ardent about creating designed, intuitive, and
+            practical products using technology.
+          </p>
+          <small>
+            (psst, read more about me in my{" "}
+            <a href="https://arash.codes" target="_blank" rel="noreferrer">
+              website
+            </a>
+            !)
+          </small>
+          <p>
+            I created <code>function()</code> as an avenue to share my code,
+            experiences, and other fascinating stories to the world. I admit
+            that I still have a long way to go to be just <i>somebody</i> in
+            this fast-paced world, but I figured: why not create a blog just to
+            document my journey along the way at the same time?
+          </p>
+          <p>
+            Since starting my journey as a developer, I had the opportunity to
+            come across many blogs by different developers out there. I was
+            inspired and intrigued at how each had its own style, as if an
+            extension of the developer behind the blog. I wanted to make a blog
+            of my own (that had actual meaning and direction) for a long time; I
+            had many failed attempts at blogging, and realised only later on
+            that a purpose was key.
+          </p>
+          <p>
+            I&apos;m still embarking on the journey of learning something new
+            (maybe not) everyday. There&apos;s still a lot for me to learn and
+            grow, but I hope that sharing what&apos;s going on through a simple
+            blog might help others who come across this blog!
+          </p>
+          <p>
+            To get started, take a look at my first blog post —{" "}
+            <Link href="/hello-world">Hello, world!</Link>. I can&apos;t thank
+            you enough for being here. I hope that you might learn a thing or
+            two, or maybe the other round of me learning from you too. I&apos;m
+            free for <a href="mailto:hello@arashnrim.me">a chat on email</a>{" "}
+            anytime, if you&apos;d like to leave a comment, give some feedback,
+            or simply say hi! Thank you for visiting; go forth and create
+            awesome things!
+          </p>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default About;
